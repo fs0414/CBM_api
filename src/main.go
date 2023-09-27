b519f1b86f58f91f5f6ae8ffa64fc89a2f3bfb92
soramar <fujitanisora0414@i.softbank.jp>
2023-09-28 02:01:09 +0900

    fix: docker auth server

--- src/main.go
+++ src/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hello bcmApi to golang")
+    fmt.Fprintf(w, "hello cbmApi to golang")
   })
 
   http.ListenAndServe(":8080", nil)

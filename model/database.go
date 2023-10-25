1e77343d4ca27bb71baccd5264f0fedb218c8287
ユーザ名 <sayasurvey@yahoo.co.jp>
2023-10-26 00:49:54 +0900

    ADD: テーブルの作成

--- model/database.go
+++ model/database.go
@@ -1,11 +1,54 @@
 package model
 
 import (
+	"time"
   "github.com/joho/godotenv"
   "gorm.io/gorm"
   "gorm.io/driver/mysql"
   "os"
 )
+
+type User struct {
+	gorm.Model
+
+	ID        uint   `gorm:"primaryKey"`
+	Name      string `gorm:"not null"`
+	Password  string `gorm:"not null"`
+	Email     string `gorm:"not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Book struct {
+	gorm.Model
+
+	ID        uint   `gorm:"primaryKey"`
+	Title     string `gorm:"not null"`
+	ImageUrl  string `gorm:"not null"`
+	Loanable  bool   `gorm:"not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type BorrowedBook struct {
+	gorm.Model
+
+	ID            uint      `gorm:"primaryKey"`
+	UserID        uint      `gorm:"not null"`
+	BookID        uint      `gorm:"not null"`
+	CheckoutDate  time.Time `gorm:"not null"`
+	ReturnDueDate time.Time `gorm:"not null"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+type BorrowingWishList struct {
+	ID        uint `gorm:"primaryKey"`
+	UserID    uint `gorm:"not null"`
+	BookID    uint `gorm:"not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
 
 func DbInit() {
   godotenv.Load(".env")
@@ -21,5 +64,5 @@ func DbInit() {
       panic("failed to connect to database")
   }
     
-  db.AutoMigrate()
+  db.AutoMigrate(&User{}, &Book{}, &BorrowedBook{}, &BorrowingWishList{})
 }
